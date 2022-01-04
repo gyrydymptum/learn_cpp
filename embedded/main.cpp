1c70106eba9c4c46e68dff89b14933bd3c8c3ddc
Gyrydymptum <gyrydymptum@gmail.com>
2022-01-04 17:34:46 +0100

    converting number to tree 8-bit

--- embedded/main.cpp
+++ embedded/main.cpp
@@ -4,21 +4,19 @@
 #include <bitset>
 
 constexpr auto fxosc = 26000000;
-
-unsigned long long int calculateFreq(unsigned long long int fcarrier ) {
-    return (fcarrier << 16)/fxosc;
-}
-std::array<uint8_t,3> freq( uint64_t calfreq) {
+constexpr size_t number_of_registers = 3;
+std::array<uint8_t,number_of_registers> freq( uint64_t fcarrier) {
+    auto calfreq = (fcarrier << 16)/fxosc;
     std::array <uint8_t,3> freqarr;
-    freqarr[0] = 1;
-    freqarr[1] = 2;
-    freqarr[2] = 3;
+    freqarr[0] = (uint8_t)(calfreq & 0xff);
+    freqarr[1] = (uint8_t)(calfreq >> 8) & 0xff;;
+    freqarr[2] = (uint8_t)(calfreq >> 16) & 0xff;
     return freqarr;
 }
 
+
 int main() {
-    std::cout << calculateFreq(868000000) << '\n';
-    auto array1 = freq(calculateFreq(868000000));
+    auto array1 = freq(868000000);
     std::for_each(array1.begin(), array1.end(), [](const auto& el) {
         std::cout << "In binary:  " << std::bitset<8>{el} << '\n';
     });
