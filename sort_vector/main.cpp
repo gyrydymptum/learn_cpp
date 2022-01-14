c1d8757026c9ae23875a78fccf9764796bd865f8
Gyrydymptum <gyrydymptum@gmail.com>
2022-01-14 10:50:35 +0100

    &

--- sort_vector/main.cpp
+++ sort_vector/main.cpp
@@ -1,30 +1,35 @@
-#include <vector>
 #include <algorithm>
-#include <memory>
 #include <iostream>
+#include <memory>
+#include <vector>
 
 template <typename T>
 void print(T toprint) {
-    for (auto el : toprint){
+    for (auto el : toprint) {
         std::cout << el << " ";
     }
     std::cout << '\n';
 }
 
-
-
-
-// void sortVec(std::vector<int> vec) {
-//     sort(vec)
-// }
+std::vector<int> sortVec(std::vector<int> & vec) {
+    std::sort(begin(vec), end(vec), [](int lhs, int rhs) {
+        if (lhs % 2 == 0 && rhs % 2 != 0) {
+            return true;
+        } else if (lhs % 2 != 0 && rhs % 2 == 0) {
+            return false;
+        } else {
+            return lhs < rhs;
+        }
+    });
+    return vec;
+}
 
 int main() {
-
-std::vector<int> vec {1,2,21,34,21,33,22,6,35,66,8,4,23,44};
-
-
-
-print(vec);
+    std::vector<int> vec{1, 2, 21, 34, 21, 33, 22, 6, 35, 66, 8, 4, 23, 44};
+    print(vec);
+    sortVec(vec);
+    print(vec);
+    print(sortVec(vec));
 
     return 0;
 }
