83e995e852cd48270f237c8fe3948cc61c390606
Gyrydymptum <gyrydymptum@gmail.com>
2022-01-18 17:57:42 +0100

    format

--- nwd-nnw/nwdNww.hpp
+++ nwd-nnw/nwdNww.hpp
@@ -1,23 +1,33 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-if (lhs<0) lhs=-lhs;
-    if (rhs<0) rhs=-rhs;
-      if (lhs==0) return rhs;
-      else if (rhs==0) return lhs;
-      else {
-        while( lhs != rhs )
-        if( lhs < rhs ) rhs -= lhs; 
-        else lhs -= rhs;
-        }return lhs;
+    if (lhs < 0)
+        lhs = -lhs;
+    if (rhs < 0)
+        rhs = -rhs;
+    if (lhs == 0)
+        return rhs;
+    else if (rhs == 0)
+        return lhs;
+    else {
+        while (lhs != rhs)
+            if (lhs < rhs)
+                rhs -= lhs;
+            else
+                lhs -= rhs;
+    }
+    return lhs;
 }
 
 int NWW(int lhs, int rhs) {
-if (lhs==0) return 0;
-    else if (rhs==0) return 0;
+    if (lhs == 0)
+        return 0;
+    else if (rhs == 0)
+        return 0;
     else {
-        int NWWresult= lhs*rhs/NWD(lhs,rhs);
-        if (NWWresult<0) return -NWWresult;
+        int NWWresult = lhs * rhs / NWD(lhs, rhs);
+        if (NWWresult < 0)
+            return -NWWresult;
         return NWWresult;
     }
 }
